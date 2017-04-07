bcb3f869cd34293c94f4081391474947065ebdbe
dianabaila <diana.baila@yahoo.com>
2017-04-08 02:35:35 +0300

    Versiune 3 (page to the map) Diana

--- itec2017/src/app/app.module.ts
+++ itec2017/src/app/app.module.ts
@@ -1,24 +1,38 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
-import { AppComponent } from './app.component';
-import { HomeComponent } from './pages/home/home.component';
-import { SubtitleComponent } from './pages/subtitle/subtitle.component';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './pages/home/home.component';
+import {SubtitleComponent} from './pages/subtitle/subtitle.component';
+import {RouterModule, Routes} from '@angular/router';
+import { PollutionMapComponent } from './pages/pollution-map/pollution-map.component';
+
+const ROUTES: Routes = [
+  {
+    path: '', component: HomeComponent
+  },
+  {
+    path: 'pollution-map', component: PollutionMapComponent
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    SubtitleComponent
+    SubtitleComponent,
+    PollutionMapComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ itec2017/src/app/pages/pollution-map/pollution-map.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pollution-map',
+  templateUrl: './pollution-map.component.html',
+  styleUrls: ['./pollution-map.component.css']
+})
+export class PollutionMapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
